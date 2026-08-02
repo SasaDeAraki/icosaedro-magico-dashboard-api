ecd9d5557a206df043303763d6767670bbdc5d44
Samuel Viana <samuel.viana.yl@gmail.com>
2026-08-02 13:48:55 -0300

    test: lancer model spec

--- app/models/lancer_character.rb
+++ app/models/lancer_character.rb
@@ -1,25 +1,25 @@
 class LancerCharacter < Character
   validates :total_pilot_hp, presence: true, numericality: { greater_than: 0 }
   validates :current_pilot_hp, presence: true, numericality: {
-    greater_than_or_equals_to: 0,
+    greater_than_or_equal_to: 0,
     less_than_or_equal_to: ->(character) { character.total_pilot_hp }
   }
 
   validates :total_structure, presence: true, numericality: { greater_than: 0 }
   validates :current_structure, presence: true, numericality: {
-    greater_than_or_equals_to: 0,
+    greater_than_or_equal_to: 0,
     less_than_or_equal_to: ->(character) { character.total_structure }
   }
 
   validates :total_frame_hp, presence: true, numericality: { greater_than: 0 }
   validates :current_frame_hp, presence: true, numericality: {
-    greater_than_or_equals_to: 0,
+    greater_than_or_equal_to: 0,
     less_than_or_equal_to: ->(character) { character.total_frame_hp }
   }
 
   validates :total_reactor, presence: true, numericality: { greater_than: 0 }
   validates :current_reactor, presence: true, numericality: {
-    greater_than_or_equals_to: 0,
+    greater_than_or_equal_to: 0,
     less_than_or_equal_to: ->(character) { character.total_reactor }
   }
 
--- app/models/ordem_character.rb
+++ app/models/ordem_character.rb
@@ -4,14 +4,14 @@ class OrdemCharacter < Character
   validates :total_pd, presence: true, numericality: { greater_than: 0 },
     if: -> { self.resource === "PD" }
   validates :current_pd, presence: true, numericality: {
-    greater_than_or_equals_to: 0,
+    greater_than_or_equal_to: 0,
     less_than_or_equal_to: ->(character) { character.total_pd }
   }, if: -> { self.resource === "PD" }
 
   validates :total_pe, presence: true, numericality: { greater_than: 0 },
     if: -> { self.resource === "PE" }
   validates :current_pe, presence: true, numericality: {
-    greater_than_or_equals_to: 0,
+    greater_than_or_equal_to: 0,
     less_than_or_equal_to: ->(character) { character.total_pe }
   }, if: -> { self.resource === "PE" }
 end
